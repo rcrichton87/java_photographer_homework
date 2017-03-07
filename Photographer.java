d602d229cd42e5de904e605cdb4f7b49ce7b4160
Ross Crichton <rcrichton87@googlemail.com>
2017-03-07 15:13:20 +0000

    cameras array list starts empty

--- Photographer.java
+++ Photographer.java
@@ -1,3 +1,15 @@
+import java.util.*;
+
 public class Photographer{
-  
+
+  private ArrayList<String> cameras;
+
+  public Photographer(){
+    this.cameras = new ArrayList<String>();
+  }
+
+  public int cameraCount(){
+    return cameras.size();
+  }
+
 }
--- PhotographerTest.java
+++ PhotographerTest.java
@@ -1,2 +1,18 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+
+public class PhotographerTest{
+
+  Photographer photographer;
+
+  @Before
+  public void before(){
+    photographer = new Photographer();
+  }
+
+  @Test
+  public void camerasStartsEmpty(){
+    assertEquals(0, photographer.cameraCount() );
+  }
+
+}
